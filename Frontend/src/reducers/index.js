247ad6ec3e015b7b6289e02c27f590de7b5bb617
Raymond Isip <raymond_isip@yahoo.com>
2019-11-03 22:42:14 +1300

    Added initial error handling when doing a rest api call

--- Frontend/src/reducers/index.js
+++ Frontend/src/reducers/index.js
@@ -1,12 +1,16 @@
-import { GET_VEHICLE_MODELS, VEHICLE_MODELS_RECEIVED } from '../actions/types';
+import { GET_VEHICLE_MODELS, VEHICLE_MODELS_RECEIVED, API_ERROR } from '../actions/types';
 
 const reducer = (state = {}, action) => {
   switch (action.type) {
     case GET_VEHICLE_MODELS: {
       return { ...state, vehicleMake: action.vehicleMake, vehicleModels: undefined, loading: true };
     }
-    case VEHICLE_MODELS_RECEIVED:
+    case VEHICLE_MODELS_RECEIVED: {
       return { ...state, vehicleModels: action.vehicleModels, loading: false };
+    }
+    case API_ERROR: {
+      return { ...state, vehicleModels: undefined, loading: false };
+    }
     default:
       return state;
   }
--- Frontend/src/sagas/index.js
+++ Frontend/src/sagas/index.js
@@ -1,9 +1,13 @@
 import { put, takeLatest, all } from 'redux-saga/effects';
-import { GET_VEHICLE_MODELS, VEHICLE_MODELS_RECEIVED } from '../actions/types';
+import { GET_VEHICLE_MODELS, VEHICLE_MODELS_RECEIVED, API_ERROR } from '../actions/types';
 
 export function* getVehicleModels(payload) {
-  const jsonResponse = yield fetch(`https://localhost:44387/vehicle-checks/makes/${payload.vehicleMake}`).then(response => response.json());
-  yield put({ type: VEHICLE_MODELS_RECEIVED, vehicleModels: jsonResponse.models });
+  try {
+    const jsonResponse = yield fetch(`https://localhost:44387/vehicle-checks/makes/${payload.vehicleMake}`).then(response => response.json());
+    yield put({ type: VEHICLE_MODELS_RECEIVED, vehicleModels: jsonResponse.models });
+  } catch (error) {
+    yield put({ type: API_ERROR, error: error });
+  }
 }
 
 export function* actionWatcher() {
